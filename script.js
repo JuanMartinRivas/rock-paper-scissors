f765c19b6cc41bcc05d30e655dca0ffa5884f2e1
JuanMartinRivas <jmartinrivas14@gmail.com>
2023-08-02 16:37:13 -0300

    Defining getPlayerChoice function
    Defining function that prompts the player to make a choice for their play

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function getComputerChoice(){
 
     return choice;
 }
+
+function getPlayerChoice(){
+    let choice;
+    let choiceNum = prompt("Make your choice:\n1.Rock.\n2.Paper.\n3.Scissors.");
+
+    if (choiceNum == "1"){
+        choice = "Rock";
+    } else if (choiceNum == "2"){
+        choice = "Paper";
+    } else if (choiceNum == "3"){
+        choice = "Scissors";
+    } else {
+        choice = null;
+    }
+
+    return choice;
+}
+
+console.log(getPlayerChoice());
