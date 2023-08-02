78a5723440293712c126dd1fecb9dd45e259e849
JuanMartinRivas <jmartinrivas14@gmail.com>
2023-08-02 16:57:02 -0300

    Defining playRound function
    Defining function that compares the computer choice with the player choice and outputs the result of the game.

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-function getComputerChoice(){
+function getComputerChoice() {
     let choice;
 
     const randNum = Math.floor(Math.random() * 3);
 
-    if (randNum == 0){
+    if (randNum == 0) {
         choice = "Rock"
-    } else if (randNum == 1){
+    } else if (randNum == 1) {
         choice = "Paper"
-    } else if (randNum == 2){
+    } else if (randNum == 2) {
         choice = "Scissors"
     };
 
     return choice;
 }
 
-function getPlayerChoice(){
+const computerChoice = getComputerChoice();
+
+function getPlayerChoice() {
     let choice;
     let choiceNum = prompt("Make your choice:\n1.Rock.\n2.Paper.\n3.Scissors.");
 
-    if (choiceNum == "1"){
+    if (choiceNum == "1") {
         choice = "Rock";
-    } else if (choiceNum == "2"){
+    } else if (choiceNum == "2") {
         choice = "Paper";
-    } else if (choiceNum == "3"){
+    } else if (choiceNum == "3") {
         choice = "Scissors";
     } else {
         choice = null;
@@ -31,4 +33,28 @@ function getPlayerChoice(){
     return choice;
 }
 
-console.log(getPlayerChoice());
+const playerChoice = getPlayerChoice();
+
+
+function playRound(playerChoice, computerChoice) {
+
+    if (playerChoice === null) {
+        return "Please select a valid option."
+    } else {
+        if (playerChoice === computerChoice) {
+            return "It's a tie!";
+        } else if (playerChoice == "Rock" && computerChoice == "Paper") {
+            return "You loose! Paper beats rock."
+        } else if (playerChoice == "Rock" && computerChoice == "Scissors") {
+            return "You win! Rock beats Scissors"
+        } else if (playerChoice == "Paper" && computerChoice == "Scissors") {
+            return "You loose! Scissors beat paper."
+        } else if (playerChoice == "Paper" && computerChoice == "Rock") {
+            return "You win! Paper beats Rock"
+        } else if (playerChoice == "Scissors" && computerChoice == "Rock") {
+            return "You loose! Rock beats Scissors."
+        } else if (playerChoice == "Scissors" && computerChoice == "Paper") {
+            return "You win! Scissors beat Paper"
+        }
+    }
+}
