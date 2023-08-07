6401b27db231a6e24ca92e74c4d3613ed1ee4d83
JuanMartinRivas <jmartinrivas14@gmail.com>
2023-08-07 16:24:57 -0300

    Bug fixing
    After fixing the bug which made all the games end in ties and creating a while loop that breaks when the player inputs a valid choise, the simple version of this js game is finally finished.

--- script.js
+++ script.js
@@ -14,11 +14,16 @@ function getComputerChoice() {
     return choice;
 }
 
-const computerChoice = getComputerChoice();
-
 function getPlayerChoice() {
     let choice;
     let choiceNum = prompt("Make your choice:\n1.Rock.\n2.Paper.\n3.Scissors.");
+
+    while (choiceNum != "1" && choiceNum != "2" && choiceNum != "3") {
+         choiceNum = prompt("Please enter one of the following choices only:\n1.Rock.\n2.Paper.\n3.Scissors.");
+         if(choiceNum == "1" || choiceNum == "2" || choiceNum == "3"){
+            break;
+         }
+    };
 
     if (choiceNum == "1") {
         choice = "Rock";
@@ -33,28 +38,34 @@ function getPlayerChoice() {
     return choice;
 }
 
-const playerChoice = getPlayerChoice();
-
-
 function playRound(playerChoice, computerChoice) {
-
-    if (playerChoice === null) {
-        return "Please select a valid option."
-    } else {
-        if (playerChoice === computerChoice) {
-            return "It's a tie!";
-        } else if (playerChoice == "Rock" && computerChoice == "Paper") {
-            return "You loose! Paper beats rock."
-        } else if (playerChoice == "Rock" && computerChoice == "Scissors") {
-            return "You win! Rock beats Scissors"
-        } else if (playerChoice == "Paper" && computerChoice == "Scissors") {
-            return "You loose! Scissors beat paper."
-        } else if (playerChoice == "Paper" && computerChoice == "Rock") {
-            return "You win! Paper beats Rock"
-        } else if (playerChoice == "Scissors" && computerChoice == "Rock") {
-            return "You loose! Rock beats Scissors."
-        } else if (playerChoice == "Scissors" && computerChoice == "Paper") {
-            return "You win! Scissors beat Paper"
-        }
+    if (playerChoice == "Rock" && computerChoice == "Paper") {
+        return "You loose! Paper beats rock."
+    } else if (playerChoice == "Rock" && computerChoice == "Scissors") {
+        return "You win! Rock beats Scissors"
+    } else if (playerChoice == "Paper" && computerChoice == "Scissors") {
+        return "You loose! Scissors beat paper."
+    } else if (playerChoice == "Paper" && computerChoice == "Rock") {
+        return "You win! Paper beats Rock"
+    } else if (playerChoice == "Scissors" && computerChoice == "Rock") {
+        return "You loose! Rock beats Scissors."
+    } else if (playerChoice == "Scissors" && computerChoice == "Paper") {
+        return "You win! Scissors beat Paper"
+    } else if (playerChoice === computerChoice) {
+        return "It's a tie!";
     }
 }
+
+function game (){
+    let pChoice;
+    let cChoice;
+
+    pChoice = getPlayerChoice();
+    cChoice = getComputerChoice();
+
+    return playRound(pChoice, cChoice);
+
+
+}
+
+console.log(game());
